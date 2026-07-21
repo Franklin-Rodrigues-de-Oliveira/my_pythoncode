6d1d5cf99b0ad94c84adbd88923e5c76da3796d5
Franklin <franklinrodrigues314@gmail.com>
2026-07-21 13:46:32 -0300

    first python code

---
+++ hello.py
@@ -0,0 +1,14 @@
+name = "Otto"
+age = 23
+gpa = 3.5
+is_student = True
+
+print(f"Hello {name}")
+print(f"You are {age} years old")
+print(f"Your gpa is {gpa}")
+
+if is_student:
+print("You are a student")
+
+else:
+print("You are NOT a student")
